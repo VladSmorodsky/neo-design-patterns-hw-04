9418db8cbfade374d8d9a4669c5370f9f2256cd5
Vlad <vlad.zah@ukr.net>
2026-04-24 09:09:29 +0300

    Implemented Prototype pattern

--- src/prototype/UserProfile.ts
+++ src/prototype/UserProfile.ts
@@ -1,1 +1,21 @@
-// TODO: реалізуйте клас або інтерфейс тут
+import { UserProfilePrototype } from "./UserProfilePrototype";
+
+export class UserProfile implements UserProfilePrototype {
+  public constructor(
+    public username: string,
+    public department: "finance" | "engineering" | "marketing",
+    public permissions: {
+      canEditUsers: boolean;
+      canApproveBudget: boolean;
+      canAccessInternalTools: boolean;
+    },
+  ) {}
+
+  clone(): UserProfilePrototype {
+    return new UserProfile(
+      this.username,
+      this.department,
+      structuredClone(this.permissions),
+    );
+  }
+}
